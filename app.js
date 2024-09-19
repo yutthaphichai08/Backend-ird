135b340e3da3133b1d65f18cd16f01170ee3938f
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-09-19 18:06:46 +0700

    clean code

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, World!");
-// });
-
 app.use("/api", contentRoute);
 
 // Start the server
