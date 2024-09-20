21931d7258d9f3f1e0b30f9ca1eab4ffadfcc63d
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-09-20 11:21:23 +0700

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const contentRoute = require("./routes/contentRoute");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", contentRoute);
